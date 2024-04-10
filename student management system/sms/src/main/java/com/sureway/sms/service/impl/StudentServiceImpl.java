5df47d1c339a81d6ae0fb1f32900d026bf9aef14
david sureway <nwachukwudavid100@gmail.com>
2024-04-10 12:42:03 +0100

    student management completed

--- student management system/sms/src/main/java/com/sureway/sms/service/impl/StudentServiceImpl.java
+++ student management system/sms/src/main/java/com/sureway/sms/service/impl/StudentServiceImpl.java
@@ -9,9 +9,10 @@ import com.sureway.sms.repository.StudentRepository;
 import com.sureway.sms.service.StudentService;
 
 
+
 @Service
 public class StudentServiceImpl implements StudentService{
-	
+
 	private StudentRepository studentRepository;
 	
 	public StudentServiceImpl(StudentRepository studentRepository) {
@@ -26,26 +27,22 @@ public class StudentServiceImpl implements StudentService{
 
 	@Override
 	public Student saveStudent(Student student) {
-		// TODO Auto-generated method stub
-		return null;
+		return studentRepository.save(student);
 	}
 
 	@Override
 	public Student getStudentById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return studentRepository.findById(id).get();
 	}
 
 	@Override
 	public Student updateStudent(Student student) {
-		// TODO Auto-generated method stub
-		return null;
+		return studentRepository.save(student);
 	}
 
 	@Override
 	public void deleteStudentById(Long id) {
-		// TODO Auto-generated method stub
-		
+		studentRepository.deleteById(id);	
 	}
 
 }
